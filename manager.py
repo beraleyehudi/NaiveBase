53082bfec72e57206d1c66d9711c568848282c51
berale <b.y.berale@gmail.com>
2025-07-27 17:11:01 +0300

    fixing pickle library syntax

--- manager.py
+++ manager.py
@@ -14,14 +14,13 @@ class Manager:
     @staticmethod
     def get_data():
         data_response = requests.get("http://v1-runing:8000/get fit data")
-        with open('data.pkl', 'rb') as f:
-            data = pickle.load(f)
+        data = pickle.loads(bytes.fromhex(data_response.json()))
+  
         return data
         
     
     @staticmethod
     def get_value_counts():
         value_counts_response = requests.get("http://v1-runing:8000/get value counts")
-        with open('value_counts.pkl', 'rb') as f:
-            value_count = pickle.load(f)
+        value_count = pickle.loads(bytes.fromhex(value_counts_response.json()))
         return value_count
