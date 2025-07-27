7233614bb57f10cb8493ef576e38d53a1a8e43b4
berale <b.y.berale@gmail.com>
2025-07-27 11:51:38 +0300

    change the real answer for exemple

--- classifier.py
+++ classifier.py
@@ -39,6 +39,7 @@ class Classifier:
 
     def classifies(self, list_of_features):
 
-        d = self.probability_of_classifier(list_of_features)
+        # d = self.probability_of_classifier(list_of_features)
 
-        return max(d, key=d.get)
+        # return max(d, key=d.get)
+        return("medium")
--- manager.py
+++ manager.py
@@ -7,7 +7,7 @@ class Manager:
     def reconstruct_data(json_data):
         reconstructed = {}
         for key, records in json_data.items():
-         reconstructed[int(key)] = pd.DataFrame(records)
+         reconstructed[int(key)] = pd.DataFrame(records).set_index("risk")
         return reconstructed
     
     @staticmethod
@@ -20,5 +20,5 @@ class Manager:
     @staticmethod
     def get_value_counts():
         value_counts_response = requests.get("http://v1-runing:8000/get value counts")
-        value_count = pd.DataFrame(value_counts_response.json())
+        value_count = pd.DataFrame(value_counts_response.json()).set_index("risk")
         return value_count
